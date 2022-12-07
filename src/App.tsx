edeed8f8996682da0d5426ce688135916ff0a794
matthew-gill <matt.gill@mention-me.com>
2022-12-07 10:25:16 +0000

    Flamingoooo

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,67 @@ import React from 'react';
 import './App.css';
 import {Trans, useTranslation} from "react-i18next";
 import "./translations/i18n";
+import {DEFAULT_THEME, RadioGroup} from "@mention-me/flamingo-ui";
+import {ThemeProvider} from "styled-components";
 
 function App() {
     const {t} = useTranslation("flycode");
     return (
-        <div>
-            <h1>Mention Me Flycode demo</h1>
+        <ThemeProvider theme={DEFAULT_THEME}>
+            <div>
+                <h1>Mention Me Flycode demo</h1>
 
-            <h2>Types of translation:</h2>
+                <h2>Types of translation:</h2>
 
-            <h3>Inline translation:</h3>
-            <p>{t("inline-translation", "This is an example of an inline translation")}</p>
+                <h3>Inline translation:</h3>
+                <p>{t("inline-translation", "This is an example of an inline translation")}</p>
 
-            <h3>Inline translation with variable:</h3>
-            <p>{t("inline-translation-with-variable", "This is an example of an inline translation with a [[{{variable}}]]", {
-                variable: "variable-value"
-            })}</p>
+                <h3>Inline translation with variable:</h3>
+                <p>{t("inline-translation-with-variable", "This is an example of an inline translation with a [[{{variable}}]]", {
+                    variable: "VARIABLE VALUE!"
+                })}</p>
 
-            <h3>Trans block</h3>
-            <Trans t={t} i18nKey="trans-block">
-                This is a trans block it spans multiple lines<br />
-                And also contains some <b>HTML</b>
-            </Trans>
-        </div>
+                <h3>Trans block</h3>
+                <Trans t={t} i18nKey="trans-block">
+                    This is a trans block it spans multiple lines<br/>
+                    And also contains some <b>HTML</b>
+                </Trans>
+
+                <h1>Some flamingo UI pieces</h1>
+
+                <RadioGroup
+                    alignment="start"
+                    orientation="row"
+                    state={{
+                        isDisabled: false,
+                        isUsingLabel: true,
+                        options: [
+                            {
+                                isDisabled: false,
+                                value: 'one',
+                                label: t('houses.mountain-goat', "Mountain goat")
+                            },
+                            {
+                                isDisabled: false,
+                                value: 'two',
+                                label: t('houses.penguin', "Penguin")
+                            },
+                            {
+                                isDisabled: false,
+                                value: 'three',
+                                label: t('houses.lassie', "Lassie")
+                            },
+                            {
+                                isDisabled: false,
+                                value: 'four',
+                                label: t('houses.flamingo', "Flamingo")
+                            }
+                        ]
+                    }}
+                    variant="default"
+                />
+            </div>
+        </ThemeProvider>
     );
 }
 
